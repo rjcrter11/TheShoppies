1f199b7f8540c6e92add4f7ae4171f6319712089
Rachel <rjcrter11@gmail.com>
2021-01-05 16:56:20 -0600

    adds functionality and delete text to search bar

--- shoppies/src/components/SearchBar/SearchBar.js
+++ shoppies/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const SearchBar = () => (
+const SearchBar = ({ handleSearchChange, searchInput, deleteSearchText }) => (
     <div className='form-container' >
         <form>
             <input
-                type='search'
-                className='search' />
+                type='text'
+                className='search'
+                value={searchInput}
+                onChange={handleSearchChange}
+            />
+            {searchInput && (
+                <button onClick={deleteSearchText}><i className="fas fa-times"></i></button>
+            )}
+
             <small>Search for a movie you would like to nominate for the Shoppies!</small>
         </form>
     </div>
--- shoppies/src/screens/HomeScreen/HomeScreen.js
+++ shoppies/src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 import SearchBar from '../../components/SearchBar/SearchBar';
 
+const movieUrl = "http://www.omdbapi.com"
+const API_KEY = "79526c77"
+
 const HomeScreen = () => {
+
+    const [movieList, setMovieList] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+
+    // Search bar functions
+    const handleSearchChange = e => {
+        setSearchInput(e.target.value)
+    }
+
+    const deleteSearchText = () => {
+        setSearchInput('')
+    }
+
+    // axios call to imdb 
+    const fetchMovies = title => {
+        axios.get(`${movieUrl}/?apikey=${API_KEY}&type=movie&s=${title}`)
+            .then(res => {
+                console.log(res);
+                setMovieList(res.data.Search);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        setMovieList(fetchMovies(searchInput));
+
+    }, [searchInput])
+
     return (
         <div className='main-container' >
-            <SearchBar />
+            <SearchBar handleSearchChange={handleSearchChange} deleteSearchText={deleteSearchText} searchInput={searchInput} />
         </div>
     )
 }
