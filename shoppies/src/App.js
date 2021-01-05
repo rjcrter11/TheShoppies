6293a7385999ed32a2d88ece74fd30156c4f7027
Rachel <rjcrter11@gmail.com>
2021-01-05 15:44:12 -0600

    fixes sass version issue sets reset and some variables makes a starter header

--- shoppies/src/App.js
+++ shoppies/src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Shoppies</h1>
+    <div>
+      <Header />
+      <main className="main-container">
+        <h1>The Shoppies</h1>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ shoppies/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className='navbar' >
+            <div className='logo-container' >
+                <h1>The Shoppies</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
