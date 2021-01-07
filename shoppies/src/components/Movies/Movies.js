40cf575cc13235d993b7c48baf29548259dbd122
Rachel <rjcrter11@gmail.com>
2021-01-06 19:52:47 -0600

    adds buttons for next and last page

--- shoppies/src/components/Movies/Movies.js
+++ shoppies/src/components/Movies/Movies.js
@@ -4,10 +4,17 @@ import MovieCard from '../MovieCard/MovieCard'
 const Movies = ({ movieList, searchInput }) => (
     <>
         <div className='movie-list-container' >
-            { searchInput.length > 0 ? (
-                <h2>Results for "{searchInput}"</h2>
+            {searchInput.length > 0 ? (
+                <div className='movie-list-container_results' >
+                    <h2>Results for "{searchInput}"</h2>
+                    <div className='movie-list-container_btns'  >
+                        <button><i className="fas fa-arrow-alt-circle-left"></i></button>
+                        <button><i className="fas fa-arrow-alt-circle-right"></i></button>
+                    </div>
+                </div>
+
             ) : null}
-            
+
 
             <div className="movie-list" >
 
