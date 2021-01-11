d8afb383dbf3f5031c12444cca1306ab83eff190
Rachel <rjcrter11@gmail.com>
2021-01-11 07:49:21 -0600

    adds darkmode styling

--- shoppies/src/components/Header/Header.js
+++ shoppies/src/components/Header/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import logo from '../../assets/shoppify.png';
+import { useDarkMode } from '../../hooks/useDarkMode';
 
-const Header = () => (
-    <nav className='navbar' >
-        <div className='navbar_logo-container' >
-            <img src={logo} alt='shoppify logo' />
-            <h1>The Shoppies</h1>
-        </div>
-    </nav>
-);
+const Header = () => {
+    const [darkMode, setDarkMode] = useDarkMode('darkMode', false);
 
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode)
+    }
+    return (
+        <nav className='navbar' >
+            <div className='navbar_logo-container' >
+                <img src={logo} alt='shoppify logo' />
+                <h1>The Shoppies</h1>
+            </div>
+            <div className='dark-mode__toggle'>
+                <div
+                    onClick={toggleMode}
+                    className={darkMode ? 'toggle toggled' : 'toggle'}
+                ></div>
+            </div>
+        </nav>
+    );
+}
 
 export default Header;
