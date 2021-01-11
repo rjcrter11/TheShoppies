458a48afb554dc184af89c629673201497229472
Rachel <rjcrter11@gmail.com>
2021-01-11 14:30:55 -0600

    adds copy link to clipboard and some styling changes

--- shoppies/src/components/NominationCard/NominationCard.js
+++ shoppies/src/components/NominationCard/NominationCard.js
@@ -2,7 +2,20 @@ import React, { useContext } from 'react';
 import { NominationsContext } from '../../contexts/NominationsContext';
 
 const NominationCard = (props) => {
-    const { handleRemoveNomination } = useContext(NominationsContext)
+    const { handleRemoveNomination } = useContext(NominationsContext);
+
+    const copyToClipBoard = (id) => {
+        const copyText = `https://www.imdb.com/title/${id}`;
+        navigator.clipboard.writeText(copyText).then(
+            () => {
+                console.log("copy Success");
+            },
+            err => {
+                console.log(err);
+            }
+        )
+    }
+
     return (
         <div className="nomination-card" >
             <div className='nomination-card_img-container' >
@@ -11,12 +24,20 @@ const NominationCard = (props) => {
             <div className='nomination-card_info' >
                 <p className='nomination-card_title' > {props.Title} </p>
             </div>
-            <button
-                onClick={() => handleRemoveNomination(props.imdbID)}
-                type='button'
-                className='nomination-card_btn'>
-                <i className='fas fa-trash' ></i>
-            </button>
+            <div className='btn-grp' >
+                <button
+                    className='nomination-card_link'
+                    onClick={() => copyToClipBoard(props.imdbID)}>
+                    <i className="fas fa-link"></i>
+                </button>
+                <button
+                    onClick={() => handleRemoveNomination(props.imdbID)}
+                    type='button'
+                    className='nomination-card_delete'>
+                    <i className='fas fa-trash' ></i>
+                </button>
+            </div>
+
         </div>
     )
 };
